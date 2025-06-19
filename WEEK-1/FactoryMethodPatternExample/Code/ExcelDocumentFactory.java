454d083f600eca2c5a04bb8874f5a3487bca869f
Pritam Chaudhari <160262341+Pritamc555@users.noreply.github.com>
2025-06-19 22:17:44 +0530

    Add files via upload

---
+++ WEEK-1/FactoryMethodPatternExample/Code/ExcelDocumentFactory.java
@@ -0,0 +1,8 @@
+package factorymethod;
+
+public class ExcelDocumentFactory extends DocumentFactory{
+	public Document createDocument() {
+		return new ExcelDocument();
+	}
+
+}
---
+++ WEEK-1/FactoryMethodPatternExample/Code/PdfDocumentFactory.java
@@ -0,0 +1,8 @@
+package factorymethod;
+
+public class PdfDocumentFactory extends DocumentFactory{
+	public Document createDocument() {
+		return new PdfDocument();
+	}
+
+}
