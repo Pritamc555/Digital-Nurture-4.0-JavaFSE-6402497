20e0a5b0510d927f3b55b4ed4d6b82a901e1b468
Pritam Chaudhari <160262341+Pritamc555@users.noreply.github.com>
2025-06-25 23:15:07 +0530

    Add files via upload

---
+++ WEEK-2/3.Setting Up JUnit/code files/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	
+	public void testAdd() {
+		Calculator calc = new Calculator();
+		int result = calc.add(5, 4);
+		assertEquals(9,result);
+	}
+
+}
