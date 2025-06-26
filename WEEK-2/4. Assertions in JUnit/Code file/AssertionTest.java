5a1b4fa05e3ad9243be41443e23d7effad626779
Pritam Chaudhari <160262341+Pritamc555@users.noreply.github.com>
2025-06-26 22:55:12 +0530

    Add files via upload

---
+++ WEEK-2/4. Assertions in JUnit/Code file/AssertionTest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AssertionTest {
+	
+	@Test
+	public void testAssertions() {
+		assertEquals(5,2+3);
+		
+		assertTrue(5 > 3);
+		
+		assertFalse(5 < 3);
+		
+		String nullValue = null;
+		assertNull(nullValue);
+		
+		String noNullValue = "Pritam";
+		assertNotNull(noNullValue);
+	}
+
+}
