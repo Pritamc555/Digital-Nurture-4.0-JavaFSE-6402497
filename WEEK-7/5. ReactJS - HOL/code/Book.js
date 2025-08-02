873e98e43e026b33e607ebbe0ac62e42f37ced01
Pritam Chaudhari <160262341+Pritamc555@users.noreply.github.com>
2025-08-02 22:09:06 +0530

    Add files via upload

---
+++ WEEK-7/5. ReactJS - HOL/code/Book.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Book() {
+  return (
+    <div className="box">
+      <h2>Book Details</h2>
+      <p>Title: Rich Dad, Poor Dad</p>
+      <p>Author:Robert T. Kiyosaki</p>
+    </div>
+  );
+}
+
+export default Book;
